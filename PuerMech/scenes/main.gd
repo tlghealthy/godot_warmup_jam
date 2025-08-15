6e5f044c757d13d07c98eecf228c7a0eaa22547d
Lynch <Lynchzor@gmail.com>
2025-08-15 15:00:39 -0700

    add tileset, basic ui stuff

--- PuerMech/scenes/main.gd
+++ PuerMech/scenes/main.gd
@@ -6,8 +6,10 @@ const BASE_RESOLUTION: Vector2 = Vector2(1920, 1080)
 # if false it's web target
 var is_desktop: bool = true
 var is_fullscreen: bool = false
-@onready var ui_root: Control = %UIRoot
+@onready var ui_root: Control = $UIRoot
 
+# if false, use room scripts
+var camera_follow = true
 
 func _ready():
 	#var is_desktop = (OS.has_feature("pc") or 
@@ -23,11 +25,13 @@ func _ready():
 	if not is_desktop:
 		print("Starting") 
 		print("Web Version Detected")
-		var hostname: String = JavaScriptBridge.eval("window.location.hostname")
+		# if you want to get where the game is running from (like itch.io)
+		# var hostname: String = JavaScriptBridge.eval("window.location.hostname")
 		await get_tree().process_frame
 		get_viewport().size_changed.connect(_apply_scale)
 		_apply_scale()
-		
+	$Player/Camera2D.make_current()
+	
 func _apply_scale():
 	var vp := get_viewport().get_visible_rect().size
 	var s : float = min(vp.x / BASE_RESOLUTION.x, vp.y / BASE_RESOLUTION.y) 
@@ -48,6 +52,8 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("SwitchWindow"):
 		is_fullscreen = not is_fullscreen
 		set_borderless_fullscreen(is_fullscreen)
+		
+	$CanvasLayer/UIRoot/YVel.text = "Velocity: " + str(floor(-1*$Player.velocity.y))
 	
 func set_borderless_fullscreen(enable: bool):
 	if enable:

e1a0e54a2e527713c2b70600296328dd87dd175e
TheLongevityGamer <tlghealthy@gmail.com>
2025-08-19 17:27:17 -0700

    Type fixed

--- PuerMech/scenes/player_weapon.gd
+++ PuerMech/scenes/player_weapon.gd
@@ -2,11 +2,12 @@ extends Node
 
 @export var weapon: Node
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	if Input.is_action_pressed("PlayerShoot") and weapon and weapon.has_method("can_fire"):
 		if weapon.can_fire():
-			var dir := Vector2.RIGHT
-			if owner is Node2D:
-				var mouse_pos := owner.get_global_mouse_position()
-				dir = (mouse_pos - (owner as Node2D).global_position).normalized()
+			var dir: Vector2 = Vector2.RIGHT
+			var owner2d: Node2D = owner as Node2D
+			if owner2d:
+				var mouse_pos: Vector2 = owner2d.get_global_mouse_position()
+				dir = (mouse_pos - owner2d.global_position).normalized()
 			weapon.fire(dir)
